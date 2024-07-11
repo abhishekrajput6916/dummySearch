83f34686b7ec7dd905577e5704092ba43ebac900
Abhishek Rajput <abhishekrajput9111606916@gmail.com>
2024-07-11 12:27:20 +0530

    made responsive

--- DummySearch/src/components/Home1.jsx
+++ DummySearch/src/components/Home1.jsx
@@ -1,13 +1,16 @@
 import React, { useEffect, useState } from 'react'
 import Navbar from './Navbar'
 import axios from 'axios';
-import { Box } from '@mui/material';
+import { Box, Button, SwipeableDrawer } from '@mui/material';
 import ProductCard from './ProductCard';
 import FilterBar from './FilterBar';
+import useMediaQuery from '@mui/material/useMediaQuery';
 
 function Home1() {
+    const matches = useMediaQuery('(min-width:600px)');
     const [productList,setProductList]=useState([]);
     const [isLoading,setIsLoading]=useState(false);
+    const [drawer, setDrawer] = React.useState(false);
     async function getData() {
         setIsLoading(true);
         try {
@@ -23,9 +26,25 @@ function Home1() {
     useEffect(()=>{
         getData();
     },[])
+    const toggleDrawer = (event) => {
+        setDrawer(event);
+        // setState({ ...state, [anchor]: open });
+      };
   return (
     <div className='container'>
-        <div className="filters"><FilterBar handleCatagory={setProductList}/></div>
+        <div className="filters">
+            {!matches?
+            <>
+            <Button onClick={()=>{toggleDrawer(true)}}>Filters</Button>
+    <SwipeableDrawer
+      open={drawer}
+      onClose={()=>{toggleDrawer(false)}}
+      onOpen={()=>{toggleDrawer(true)}}
+      sx={{textAlign:'center'}}
+    >
+      <FilterBar handleCatagory={setProductList}/>
+    </SwipeableDrawer></>:<FilterBar handleCatagory={setProductList}/>}
+            </div>
     <Box className='home' sx={{mt:'1rem'}}>
         {!isLoading?productList.map(product=>{
             return <ProductCard product={product}/>
